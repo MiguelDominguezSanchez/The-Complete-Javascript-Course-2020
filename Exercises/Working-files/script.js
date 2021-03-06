73af29a3a8fe8e82944f78a8d54184ea1c06ed15
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-06 18:43:47 +0100

    29. Coding Challenge 5 Solution

--- Exercises/Working-files/script.js
+++ Exercises/Working-files/script.js
@@ -388,7 +388,7 @@ var john = {
 			var percentage
 			var bill = this.bills[i]
 
-			if (bill[i] < 50) {
+			if (bill < 50) {
 				percentage = 0.2
 			} else if (bill >= 50 && bill < 200) {
 				percentage = 0.15
@@ -402,5 +402,60 @@ var john = {
 	},
 }
 
+var mark = {
+	fullName: 'Mark Miller',
+	bills: [77, 475, 110, 110, 45],
+	calcTips: function () {
+		this.tips = []
+		this.finalValues = []
+
+		for (var i = 0; i < this.bills.length; i++) {
+			// Determine percentage based on tipping rules
+			var percentage
+			var bill = this.bills[i]
+
+			if (bill < 100) {
+				percentage = 0.2
+			} else if (bill >= 100 && bill < 300) {
+				percentage = 0.1
+			} else {
+				percentage = 0.25
+			}
+			// Add results to the corresponding arrays
+			this.tips[i] = bill * percentage
+			this.finalValues[i] = bill + bill * percentage
+		}
+	},
+}
+
+function calcAverage(tips) {
+	var sum = 0
+	for (var i = 0; i < tips.length; i++) {
+		sum = sum + tips[i]
+	}
+	return sum / tips.length
+}
+// [2, 6, 4] -> 0 / 2 / 8 / 12
+
+// Do the calculations
 john.calcTips()
-console.log(john)
+mark.calcTips()
+// console.log(john, mark)
+
+john.average = calcAverage(john.tips)
+mark.average = calcAverage(mark.tips)
+console.log(john, mark)
+
+if (john.average > mark.average) {
+	console.log(
+		john.fullName +
+			"'s family pays higher tips, with an average of $" +
+			john.average
+	)
+} else if (mark.average > john.average) {
+	console.log(
+		mark.fullName +
+			"'s family pays higher tips, with an average of $" +
+			mark.average
+	)
+}
