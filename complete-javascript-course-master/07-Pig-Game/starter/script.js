63eb049f3fdcf75ed4a8e89de71e3fbb68c09ee2
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-07 13:35:05 +0100

    4.8 Updating Scores and Changing the Active Player

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -14,7 +14,7 @@ var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -32,6 +32,28 @@ document.querySelector('.btn--roll').addEventListener('click', function () {
   diceDOM.src = 'dice-' + dice + '.png';
 
   // 3. Update the round score IF the rolled number ws NOT a 1
+  if (dice !== 1) {
+    // Add score
+    roundScore += dice;
+    document.querySelector(
+      '#current--' + activePlayer
+    ).textContent = roundScore;
+  } else {
+    // Next player
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    roundScore = 0;
+
+    document.getElementById('current--0').textContent = '0';
+    document.getElementById('current--1').textContent = '0';
+
+    document.querySelector('.player--0').classList.toggle('player--active');
+    document.querySelector('.player--1').classList.toggle('player--active');
+
+    // document.querySelector('.player--0').classList.remove('player--active');
+    // document.querySelector('.player--1').classList.add('player--active');
+
+    document.querySelector('.dice').style.display = 'none';
+  }
 });
 
 // document.querySelector('#current--' + activePlayer).textContent = dice;
