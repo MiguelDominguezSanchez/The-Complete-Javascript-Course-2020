4a13ead4cef4cbf1fff1ef440963c1d4fc02ce39
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-07 09:26:12 +0100

    3.7 Scoping and the Scope chain

--- JS-Basics/script.js
+++ JS-Basics/script.js
@@ -468,34 +468,73 @@ while (i < john.length) {
 ////////////////////////////////////////////
 // Lecture: Hoisting
 
-// functions
-calculateAge(1965)
+// // functions
+// calculateAge(1965)
 
-// function declaration // Hoisting only works for function declarations
-function calculateAge(year) {
-	console.log(2016 - year)
+// // function declaration // Hoisting only works for function declarations
+// function calculateAge(year) {
+// 	console.log(2016 - year)
+// }
+
+// // calculateAge(1990)//
+
+// // Function Expression
+// // retirement(1956) // Not Hoisted
+// var retirement = function (year) {
+// 	console.log(65 - (2016 - year))
+// }
+
+// // retirement(1990)
+
+// // variables
+
+// console.log(age)
+// var age = 23
+// console.log(age)
+
+// function foo() {
+// 	console.log(age)
+// 	var age = 65
+// 	console.log(age)
+// }
+// foo()
+// console.log(age)
+
+////////////////////////////////////////////
+// Lecture: Scoping
+
+// First scoping example
+
+// var a = 'Hello'
+// first()
+
+// function first() {
+// 	var b = 'Hi!'
+// 	second()
+
+// 	function second() {
+// 		var c = 'Hey!'
+// 		console.log(a + b + c)
+// 	}
+// }
+
+//
+
+var a = 'Hello!'
+first()
+
+function first() {
+	var b = 'Hi!'
+	second()
+
+	function second() {
+		var c = 'Hey!'
+		third()
+	}
 }
 
-// calculateAge(1990)//
-
-// Function Expression
-// retirement(1956) // Not Hoisted
-var retirement = function (year) {
-	console.log(65 - (2016 - year))
+function third() {
+	var d = 'John'
+	// console.log(c)
+	console.log(a + d)
 }
-
-// retirement(1990)
-
-// variables
-
-console.log(age)
-var age = 23
-console.log(age)
-
-function foo() {
-	console.log(age)
-	var age = 65
-	console.log(age)
-}
-foo()
-console.log(age)
