b502f991dc52d361d379b06d0050f6d72f6579ca
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-12 09:53:14 +0100

    7.6 Strings in ES6 ES2015

--- complete-javascript-course-1/7-ES6/starter/script.js
+++ complete-javascript-course-1/7-ES6/starter/script.js
@@ -80,7 +80,7 @@ console.log(i)
 // Lecture: Blocks and IIFEs
 
 // Immediately invoked function expressions
-
+/*
 // ES6
 {
 	const a = 1
@@ -96,3 +96,40 @@ console.log(c)
 })()
 
 // console.log(c)
+*/
+
+////////////////////////////////////////////
+// Lecture: Strings
+
+let firstName = 'John'
+let lastName = 'Smith'
+const yearOfBirth = 1990
+function calcAge(year) {
+	return 2016 - year
+}
+
+// ES5
+console.log(
+	'This is ' +
+		firstName +
+		' ' +
+		lastName +
+		'. He was born in ' +
+		yearOfBirth +
+		'. Today, he is ' +
+		calcAge(yearOfBirth) +
+		' years old.'
+)
+
+//  ES6
+console.log(
+	`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today he is ${calcAge(
+		yearOfBirth
+	)} years old.`
+)
+
+const n = `${firstName} ${lastName}`
+console.log(n.startsWith('J'))
+console.log(n.endsWith('Sm'))
+console.log(n.includes('oh'))
+console.log(`${firstName}`.repeat(5))
